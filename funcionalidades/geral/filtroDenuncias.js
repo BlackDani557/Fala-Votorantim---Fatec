d881a03dbf223746432b0784f6dc70c3f1772146
DANIEL DE FREITAS ESTEVES DA COSTA <143741099+BlackDani557@users.noreply.github.com>
2026-06-03 16:19:25 -0300

    tes
    test

--- funcionalidades/geral/filtroDenuncias.js
+++ funcionalidades/geral/filtroDenuncias.js
@@ -1,5 +1,5 @@
 //Puxar filtro para Feed de Denúncias
-fetch('/componentes/geral/filtroDenuncias.html')
+fetch('../../componentes/geral/filtroDenuncias.html')
 .then((resposta) => resposta.text())
 .then((dados) => {
     document.getElementById('filtros').innerHTML = dados;
